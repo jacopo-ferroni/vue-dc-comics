98ae2ca3c1cdcd75070f0b388a59369e2610c784
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-11-24 17:01:00 +0100

    stuck

---
+++ src/components/Games.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <img :src="img" :alt="text">
+    <div>{{text}}</div> 
+</div>
+</template>
+
+<script>
+export default {
+    name : `Games`,
+    props : {
+        img : String,
+        text : String,
+        price : String,
+    }
+}
+</script>
+
+<style>
+
+</style>
